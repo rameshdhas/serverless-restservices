5dd1398730053b03190f7745fab3932695a4aff0
Ramesh Dhason <drameshdhas@gmail.com>
2019-10-09 21:17:56 -0400

    Added spring boot

---
+++ src/main/java/com/leaggo/serverless/SpringBootRestLambdaHandler.java
@@ -0,0 +1,26 @@
+package com.leaggo.serverless;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class SpringBootRestLambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse>{
+
+	private static SpringLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler = null;
+	
+	static{
+		try {
+			handler = SpringLambdaContainerHandler.getAwsProxyHandler(SpringConfig.class);
+		} catch (ContainerInitializationException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public AwsProxyResponse handleRequest(AwsProxyRequest awsProxyRequest, Context context) {
+		return handler.proxy(awsProxyRequest, context);
+	}
+}
